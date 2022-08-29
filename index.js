bd0e25f766ecd2f5e996be160ce8810a10a76f61
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-29 13:27:34 -0400

    commit tree a195ce2e24b4ec895214ef122a0eda83557d20a7

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400) {
+    return "This one is on me!";
+  }
+    else if (number >= 400 && number <= 2000) {
+    return "That will be twenty bucks.";
+  }   else if (number >= 2000 && number < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }   else if (number >= 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  }
+  else if (city !== 'NYC') {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return "Thank you so much."
+  } else if (tip === "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
